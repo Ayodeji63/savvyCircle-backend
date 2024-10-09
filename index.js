7435e81cd842e5d22e2fcb2c84d0cee3ed0e308a
Ayodeji <olusanyasamson633@gmail.com>
2024-10-09 22:35:33 +0100

    changes4

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${PORT}`)
 })
 
 
